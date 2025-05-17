9bbfb461c9725695beda7586576becb4bfacc988
Akalya <rasikaakalya@gmail.com>
2025-05-17 15:50:25 +0530

    day3

---
+++ Desktop/Full Stack/day 4/statements.js
@@ -0,0 +1,72 @@
+// n= 10
+// if (n % 2 == 0) {
+//     console.log("even")
+//}
+// els {
+//    console.log("odd")
+// }
+//
+// n = 1;
+// while ( n <= 10) {
+//    if (n % 2 == 0) {
+//        console.log(`${n} is Even`)
+//    }
+//    else {
+//        covsole.log(`${n} is odd`)
+//    }
+//    n++;
+// }
+
+//n = 1;
+//do {
+//    if (n % 2 === 0) {
+//        console.log(`${n} is Even`)
+//   }
+//   else {
+//       console.log(`${n} is odd`)
+//  }
+//  n++
+//} while (n <= 10);
+
+//n = 7
+//switch (n) {
+//  case 1:
+//      console.log("suday")
+//       break;
+//  case 2:
+//      console.log("momday")
+//      break;
+//  case 3:
+//      console.log("tuesday")
+//      break;
+//    case 4:
+//        console.log("wednesday")
+//        break;
+//  case 5:
+//      console.log("thursday")
+//      break;
+//  case 6:
+//      console.log("friday")
+//      break;
+//  case 7:
+//      console.log("saturday")
+//        break;
+//    default:
+//        console.log("enter the valid number")
+//} 
+
+// const sum =() => {
+//   console.log(10 + 20)
+// }
+// sum()
+
+// var element = DocumentFragment. createElement ("div")
+// element.setAttribute("class" ,"main")
+// element.innerText = "this is element from"
+
+// document .body.append(element);
+
+
+n = 10
+var result = (n % 2 === 0) ? "Even" : "odd"
+console.log(result)
