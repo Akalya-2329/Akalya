0307af91617cdc1b5aa190cf8aad200a7fda27f7
Akalya <rasikaakalya@gmail.com>
2025-05-23 16:03:07 +0530

    day6

--- Desktop/Full Stack/akalya/rasika/akalya/urls.py
+++ Desktop/Full Stack/akalya/rasika/akalya/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import homepage
+from .views import *
 
 
 urlpatterns=[
-    path ('home/',homepage)
-]
+    path ('home/',homepage),
+    path('student_form/', student_form, name='student_form'),
+    path('signup/',signup_view, name= 'signup'),
+    path('login/',login_view, name= 'login'),
+] 
--- Desktop/Full Stack/akalya/rasika/akalya/views.py
+++ Desktop/Full Stack/akalya/rasika/akalya/views.py
@@ -12,8 +12,54 @@ def student_form (request):
         name=request.POST.get('name')
         roll_no=request.POST.get('roll_no')
         email=request.POST.get('email')
-        student.objects.creat(name=name,roll_no=roll_no,email=email)
-        return redirect('student_from')
+        student.objects.create(name=name,roll_no=roll_no,email=email)
+        return redirect('student_form')
+    
+
     students=student.objects.all()
     return render(request,'index.html',{'students':students})
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+ 
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password =request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request,"Username already exists.")
+            return redirect('signup')
+        
+        user =User.objects.create_user(username=username,eamil=email,password=password)
+        messages.success(request, "account created successfully")
+        return redirect('login')
+    
+    return render(request,'signup.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,  username= username,password =password, )
+        if User:
+            login(request, user)
+            return redirect('student_form')
+        else:
+            messages.error(request, "Invalid credentials.")
+            return redirect('login')
+        
+        return render(request,'login.html')
+        
+
+
+
+
+
+        
 
